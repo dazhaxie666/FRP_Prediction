50403c2b753e55890f18b8f657308ab0b600f663
董子勋 <101981022+dazhaxie666@users.noreply.github.com>
2024-01-08 17:55:46 +0800

    Update Multimodal Encoder.py

--- Deep Learning Model/Multimodal Encoder.py
+++ Deep Learning Model/Multimodal Encoder.py
@@ -42,7 +42,55 @@ class StateBlock(nn.Module):
         x = torch.relu(self.conv3(x))
         return x
 
-# More classes...
+class DynamicBlock(nn.Module):
+     """
+    DynamicBlock for processing sequential data using ConvLSTM and Conv layers.
+    """
+    def __init__(self, input_channels, convlstm_hidden_channels, conv_hidden_channels, device):
+        super(DynamicBlock, self).__init__()
+        self.device = device
+        hidden_dims = [convlstm_hidden_channels] * 4
+
+        # ConvLSTM layers
+        self.convlstm = ConvLSTM(input_dim=input_channels,
+                                 hidden_dim=hidden_dims,
+                                 kernel_size=[(5, 5), (3, 3), (3, 3), (1, 1)],
+                                 num_layers=4,
+                                 batch_first=True,
+                                 bias=True,
+                                 return_all_layers=False).to(device)
+
+        # Convolutional layers for processing the output of the last ConvLSTM layer
+        self.conv1 = nn.Conv2d(in_channels=convlstm_hidden_channels, out_channels=conv_hidden_channels[0], kernel_size=3, padding=1).to(device)
+        self.conv2 = nn.Conv2d(in_channels=conv_hidden_channels[0], out_channels=conv_hidden_channels[1], kernel_size=3, padding=1).to(device)
+        self.conv3 = nn.Conv2d(in_channels=conv_hidden_channels[1], out_channels=conv_hidden_channels[2], kernel_size=1, padding=0).to(device)
+
+    def forward(self, x):
+        # ConvLSTM forward pass
+        x, _ = self.convlstm(x)
+        x = x[0]  # Get the output of the last ConvLSTM layer
+        x = x[:, -1, :, :, :]  # Select the output of the last timestep
+        
+        # Forward pass through Conv layers
+        x = torch.relu(self.conv1(x))
+        x = torch.relu(self.conv2(x))
+        x = torch.relu(self.conv3(x))
+
+        return x
+class ConstantBlock(nn.Module):
+     """
+   ConstantBlock for processing spatial data using Conv layers.
+    """
+    def __init__(self, input_channels):
+        super(ConstantBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=input_channels, out_channels=8, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, padding=1)
+    def forward(self, x):
+        x = torch.relu(self.conv1(x))
+        x = torch.relu(self.conv2(x))
+        x = torch.relu(self.conv3(x))
+        return x
 
 # Testing routine
 def test_blocks():
